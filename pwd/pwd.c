e205a3cc53bdfc557f61a0af644323aa4236b69a
Shamim <abir09bs@gmail.com>
2024-04-16 23:02:25 +0200

    add pwd code

---
+++ pwd/pwd.c
@@ -0,0 +1,21 @@
+#include <errno.h>  // errno
+#include <stdio.h>  // printf
+#include <stdlib.h> // EXIT_SUCCESS
+#include <string.h> // strerror
+#include <unistd.h> // getcwd
+
+int main(void) {
+    size_t n = 4096;
+    char* buf = malloc(n);
+
+    if (!getcwd(buf, n)) {
+        printf("%s.\n", strerror(errno));
+        free(buf);
+        return EXIT_FAILURE;
+    }
+
+    printf("%s\n", buf);
+
+    free(buf);
+    return EXIT_SUCCESS;
+}
